def88b83572d3bb76039899e9bb04c40a4dac9f9
AndrewH6 <76458133+AndrewH6@users.noreply.github.com>
2021-09-17 13:06:10 -0600

    Update BioCG_catcontvarsum.r

--- R/BioCG_catcontvarsum.r
+++ R/BioCG_catcontvarsum.r
@@ -12,7 +12,7 @@ the_var_index <- getcol(data_mat,var_name)
 if(cat_var == TRUE){
   catvarsum(data_mat,the_var_index,the_var_index)
 } else if(cat_var == FALSE){
-  catcontvarsum(data_mat,the_var_index,the_var_index)
+  contvarsum(data_mat,the_var_index,the_var_index)
 }
 
 }
